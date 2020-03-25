bcd905c3112d93c19349fade6cab07a44e9398a6
Sir Yaw <samappjnr@aol.com>
2020-03-25 15:09:10 -0400

    fixed port issue

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 const app = express();
-const PORT = process.env.PORT || 0000;
+const PORT = process.env.PORT || 8080;
 
 let notesData = [];
 
